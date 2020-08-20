966a96a3bd2f5c3d67188415d1455c2e5a22233e
SreeChandraVaransi <sreechandravaransi@gamil.com>
2020-08-20 17:59:41 +0530

    updated Workbench

--- Dump20200820.sql
+++ Dump20200820.sql
@@ -23,9 +23,11 @@ DROP TABLE IF EXISTS `artist`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `artist` (
-  `email` varchar(30) NOT NULL,
-  `a_name` varchar(20) DEFAULT NULL,
-  `phone` decimal(10,0) DEFAULT NULL,
+  `email` varchar(45) NOT NULL,
+  `a_name` varchar(45) DEFAULT NULL,
+  `pwd` varchar(45) DEFAULT NULL,
+  `wallet` decimal(10,0) DEFAULT NULL,
+  `count` int DEFAULT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,7 +38,7 @@ CREATE TABLE `artist` (
 
 LOCK TABLES `artist` WRITE;
 /*!40000 ALTER TABLE `artist` DISABLE KEYS */;
-INSERT INTO `artist` VALUES ('kalyan@gmail.com','kalyan',9441556781),('new@gmail.com','new',NULL),('ram@gmail.com','ram',9900886677),('sara@gmail.com','sarath',9998885563),('shiv@gmail.com','Shivani Singh',8765432123),('siri@gmail.com','Siri',9876543212),('vidya@gmail.com','vidya',9988776655),('wert@gmail.com','sdfs',9988776655);
+INSERT INTO `artist` VALUES ('aalu@gmail.com','aalu','aalu123',0,0),('kalyan@gmail.com','kalyan',NULL,NULL,NULL),('new@gmail.com','new',NULL,NULL,NULL),('raj@gmail.com','raj',NULL,NULL,NULL),('ram@gmail.com','ram',NULL,NULL,NULL),('sara@gmail.com','sarath',NULL,NULL,NULL),('shiv@gmail.com','Shivani Singh',NULL,NULL,NULL),('shiva@gmail.com','sjd',NULL,NULL,NULL),('siri@gmail.com','Siri',NULL,NULL,NULL),('vidya@gmail.com','vidya',NULL,NULL,NULL),('wert@gmail.com','sdfs',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `artist` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,11 +50,12 @@ DROP TABLE IF EXISTS `bought_by`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bought_by` (
-  `c_id` int DEFAULT NULL,
-  `url` varchar(50) DEFAULT NULL,
-  `cost` int DEFAULT NULL,
-  KEY `c_id` (`c_id`),
-  CONSTRAINT `bought_by_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `customer` (`c_id`)
+  `url` varchar(1000) DEFAULT NULL,
+  `c_email` varchar(45) DEFAULT NULL,
+  KEY `fk_url_bought_idx` (`url`),
+  KEY `fk_email_customer_idx` (`c_email`),
+  CONSTRAINT `fk_email_customer` FOREIGN KEY (`c_email`) REFERENCES `customer` (`c_email`),
+  CONSTRAINT `fk_url_bought` FOREIGN KEY (`url`) REFERENCES `painting` (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -73,11 +76,10 @@ DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `customer` (
-  `c_id` int NOT NULL,
-  `c_name` varchar(20) DEFAULT NULL,
-  `address` varchar(50) DEFAULT NULL,
-  `phone` decimal(10,0) DEFAULT NULL,
-  PRIMARY KEY (`c_id`)
+  `c_name` varchar(45) DEFAULT NULL,
+  `c_email` varchar(45) NOT NULL,
+  `pwd` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`c_email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -91,33 +93,6 @@ LOCK TABLES `customer` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `exhibition`
---
-
-DROP TABLE IF EXISTS `exhibition`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `exhibition` (
-  `e_id` int NOT NULL,
-  `e_name` varchar(10) DEFAULT NULL,
-  `e_type` int DEFAULT NULL,
-  `start_date` date DEFAULT NULL,
-  `end_date` date DEFAULT NULL,
-  PRIMARY KEY (`e_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `exhibition`
---
-
-LOCK TABLES `exhibition` WRITE;
-/*!40000 ALTER TABLE `exhibition` DISABLE KEYS */;
-INSERT INTO `exhibition` VALUES (123,'NAMPALLY',1,'2020-08-23','2020-08-25');
-/*!40000 ALTER TABLE `exhibition` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `gallery`
 --
 
@@ -125,13 +100,11 @@ DROP TABLE IF EXISTS `gallery`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `gallery` (
-  `g_id` int NOT NULL,
-  `g_name` varchar(20) DEFAULT NULL,
-  `landmark` varchar(15) DEFAULT NULL,
-  `city` varchar(10) DEFAULT NULL,
-  `url` varchar(1000) DEFAULT NULL,
-  PRIMARY KEY (`g_id`),
-  KEY `gallery_ibfk_1` (`url`)
+  `g_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`g_name`),
+  KEY `fk_email_idx` (`email`),
+  CONSTRAINT `fk_email` FOREIGN KEY (`email`) REFERENCES `artist` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -141,6 +114,7 @@ CREATE TABLE `gallery` (
 
 LOCK TABLES `gallery` WRITE;
 /*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
+INSERT INTO `gallery` VALUES ('aalu1','aalu@gmail.com');
 /*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,12 +126,13 @@ DROP TABLE IF EXISTS `likes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `likes` (
-  `url` varchar(2000) DEFAULT NULL,
-  `title` varchar(50) NOT NULL,
-  `c_id` int DEFAULT NULL,
-  `c_name` varchar(20) DEFAULT NULL,
+  `url` varchar(1000) DEFAULT NULL,
+  `c_email` varchar(45) DEFAULT NULL,
   `count` int DEFAULT NULL,
-  PRIMARY KEY (`title`)
+  KEY `fk_url_idx` (`url`),
+  KEY `fk_email_idx` (`c_email`),
+  CONSTRAINT `fk_email_likes` FOREIGN KEY (`c_email`) REFERENCES `customer` (`c_email`),
+  CONSTRAINT `fk_url` FOREIGN KEY (`url`) REFERENCES `painting` (`url`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -178,9 +153,9 @@ DROP TABLE IF EXISTS `login`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `login` (
-  `email` varchar(50) NOT NULL,
+  `email` varchar(45) NOT NULL,
   `usertype` int DEFAULT NULL,
-  `pwd` varchar(10) DEFAULT NULL,
+  `pwd` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -191,34 +166,8 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES ('abhisign@gmail.com',2,'singup'),('cust@gmail.com',2,'1234'),('kalyan@gmail.com',1,'kalyan123'),('new@gmail.com',1,'1234'),('ram@gmail.com',1,'ram'),('sara@gmail.com',1,'sara123'),('shiv123@gmail.com',2,'11223'),('shiv@gmail.com',1,'2345'),('siri@gmail.com',1,'1234'),('vidya@gmail.com',1,'34567'),('wert@gmail.com',1,'1234');
+INSERT INTO `login` VALUES ('abhisign@gmail.com',2,'singup'),('chandra@gmail.com',2,'chandra'),('cust@gmail.com',2,'1234'),('kalyan@gmail.com',1,'kalyan123'),('new@gmail.com',1,'1234'),('ram@gmail.com',1,'ram'),('sara@gmail.com',1,'sara123'),('shiv123@gmail.com',2,'11223'),('shiv@gmail.com',1,'2345'),('siri@gmail.com',1,'1234'),('vidya@gmail.com',1,'34567'),('wert@gmail.com',1,'1234');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `organise`
---
-
-DROP TABLE IF EXISTS `organise`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `organise` (
-  `g_id` int DEFAULT NULL,
-  `e_id` int DEFAULT NULL,
-  KEY `g_id` (`g_id`),
-  KEY `e_id` (`e_id`),
-  CONSTRAINT `organise_ibfk_1` FOREIGN KEY (`g_id`) REFERENCES `gallery` (`g_id`),
-  CONSTRAINT `organise_ibfk_2` FOREIGN KEY (`e_id`) REFERENCES `exhibition` (`e_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `organise`
---
-
-LOCK TABLES `organise` WRITE;
-/*!40000 ALTER TABLE `organise` DISABLE KEYS */;
-/*!40000 ALTER TABLE `organise` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -229,13 +178,14 @@ DROP TABLE IF EXISTS `painting`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `painting` (
-  `title` varchar(50) NOT NULL,
-  `id` varchar(50) DEFAULT NULL,
-  `year` year DEFAULT NULL,
   `url` varchar(1000) NOT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`url`)
+  `title` varchar(45) DEFAULT NULL,
+  `year` varchar(45) DEFAULT NULL,
+  `g_name` varchar(45) DEFAULT NULL,
+  `cost` decimal(5,0) DEFAULT NULL,
+  PRIMARY KEY (`url`),
+  KEY `fk_g_name_idx` (`g_name`),
+  CONSTRAINT `fk_g_name` FOREIGN KEY (`g_name`) REFERENCES `gallery` (`g_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -245,7 +195,7 @@ CREATE TABLE `painting` (
 
 LOCK TABLES `painting` WRITE;
 /*!40000 ALTER TABLE `painting` DISABLE KEYS */;
-INSERT INTO `painting` VALUES ('krishna','abc',2020,'https://d2qs8jo5daq3wi.cloudfront.net/photos/275625/full/IMG_20200310_140012.jpg?1584423571','Oil painting','kalyan@gmail.com'),('Ganesh','abc',2015,'https://images-na.ssl-images-amazon.com/images/I/51xTq3TuzAL._SY450_.jpg','Oil painting',NULL),('butterfly','abc',2018,'https://storage.googleapis.com/ehimages/2020/3/18/img_b40c24bec4e449a375b7c0c28168b553_1584542046663_processed_original.jpg','Oil painting','new@gmail.com');
+INSERT INTO `painting` VALUES ('www.google.com','rose','2020','aalu1',NULL),('www.google.comfjdfjhjkff','jasmine','2020','aalu1',NULL),('www.google.comfjdfjhjkfgghff','jasmine123','2020','aalu1',NULL);
 /*!40000 ALTER TABLE `painting` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -258,4 +208,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-20 15:25:19
+-- Dump completed on 2020-08-20 17:57:49
